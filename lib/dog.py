d3a4256dc182d9536889ca560d7592804d8632c7
Sarah Whalley <s.whalley1130@gmail.com>
2023-03-24 12:59:55 -0600

    Completes lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,25 +1,30 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def new_from_db(session):
     pass
 
 def get_all(session):
-    pass
+    query=session.query(Dog).all()
+    return query
 
 def find_by_name(session, name):
-    pass
+    query=session.query(Dog).filter(Dog.name==name).first()
+    return query
 
 def find_by_id(session, id):
-    pass
+    query=session.query(Dog).filter(Dog.id==id).first()
+    return query
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    query=session.query(Dog).filter(Dog.name==name, Dog.breed==breed).first()
+    return query
 
 def update_breed(session, dog, breed):
-    pass
+    return session.query(Dog).update({Dog.breed:breed})
--- lib/models.py
+++ lib/models.py
@@ -6,4 +6,14 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Dog(Base):
-    pass
+    __tablename__="dogs"
+    
+    id=Column(Integer(), primary_key=True)
+    name=Column(String())
+    breed=Column(String())
+
+    def __repr__(self):
+        return f"Dog Name: {self.name}, "\
+                +f"Dog Breed: {self.breed}, "\
+                +f"Dog ID: {self.id}."
+    
